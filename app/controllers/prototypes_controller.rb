7e24848635a611de7d04ffc09257d2b3e6c7edae
kim-shun <kimura1417080@gmail.com>
2021-07-25 17:09:30 +0900

    詳細ページの実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -17,10 +17,11 @@ class PrototypesController < ApplicationController
   end
 
   def show
+    @prototype = Prototype.find(params[:id])
   end
 
   private
-  
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image ).merge(user_id: current_user.id)
   end
